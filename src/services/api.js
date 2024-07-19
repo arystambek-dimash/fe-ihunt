3c12bef31a0192ba4b9d54b5568ab5801d59bded
dinmukhammed <artystambekdimash005@gmail.com>
2024-07-19 20:55:21 +0500

    feat: created base url and interceptors

---
+++ src/services/api.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:8000/api/v1',
+});
+
+const refreshAccessToken = async () => {
+    const refreshToken = localStorage.getItem('refreshToken');
+    if (!refreshToken) {
+        window.location.href = '/login';
+        throw new Error('No refresh token available');
+    }
+    console.log(refreshToken)
+    const response = await axios.post('http://localhost:8000/api/v1/users/refresh-token', {refreshToken});
+    const {accessToken} = response.data;
+
+    localStorage.setItem('accessToken', accessToken);
+
+    return accessToken;
+};
+
+api.interceptors.request.use(async (config) => {
+    let token = localStorage.getItem('accessToken');
+    const refreshToken = localStorage.getItem('refreshToken');
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    } else if (refreshToken) {
+        try {
+            token = await refreshAccessToken();
+            config.headers.Authorization = `Bearer ${token}`;
+        } catch (error) {
+            console.error('Error refreshing access token', error);
+            return Promise.reject(error);
+        }
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+export default api;
