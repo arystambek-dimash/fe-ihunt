f0c07fd2a1b317b374500a58fd262eda732affe4
arystambek-dimash <arystambekdimash005@gmail.com>
2024-07-29 02:15:33 +0500

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ import LinkedInCallbackPage from "./pages/LinkedinCallbackPage";
 import HrMainPage from "./pages/HrMainPage";
 import HrNavbar from "./components/HrNavbar";
 import ComingSoonPage from "./pages/ComingSoonPage";
+import {Analytics} from "@vercel/analytics/react";
 
 const AppContent = () => {
     const location = useLocation();
@@ -47,6 +48,7 @@ const AppContent = () => {
 
     return (
         <>
+            <Analytics/>
             {!isNotFoundPage && (showLandingNavbar ? <LandingNavbar/> : showAutoResponseNavbar ?
                 <AutoResponseNavbar/> : showHrNavbar ? <HrNavbar/> : null)}
             <Routes>
